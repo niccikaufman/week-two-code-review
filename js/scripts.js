b0f176c113f24a42a202b809e59a4acda8aee066
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-25 16:04:17 -0700

    update functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,30 @@
-var quizResults = function(input1,input2,input3,input4,input5,input6,input7){
-  var result = input1 + input2 + input3 + input4 + input5 + input6 + input7;
-  return result;
-};
-
 $(document).ready(function() {
   $("#submit").click(function(){
-    var q1Input = parseInt($("input:radio.question1:checked").val());
-    var q2Input = parseInt($("input:radio.question2:checked").val());
-    var q3Input = parseInt($("input:radio.question3:checked").val());
-    var q4Input = parseInt($("input:radio.question4:checked").val());
-    var q5Input = parseInt($("input:radio.question5:checked").val());
-    var q6Input = parseInt($("input:radio.question6:checked").val());
-    var q7Input = parseInt($("input:radio.question7:checked").val());
+    var result = parseInt($("input:radio.question1:checked").val()) + parseInt($("input:radio.question2:checked").val()) + parseInt($("input:radio.question3:checked").val()) + parseInt($("input:radio.question4:checked").val()) + parseInt($("input:radio.question5:checked").val()) + parseInt($("input:radio.question6:checked").val()) + parseInt($("input:radio.question7:checked").val());
+    console.log(result);
 
-    var questionOutput = quizResults(q1Input,q2Input,q3Input,q4Input,q5Input,q6Input,q7Input);
-
-    if (questionOutput <= 7){
+    if (result <= 7){
       $(".results").show();
       $("#quiz-result").text("lazy");
       $("#lazy").show();
       $("#submit").toggle();
 
     }
-    else if (questionOutput <= 21 && questionOutput > 8){
+    else if (result <= 21 && result > 8){
       $(".results").show();
       $("#quiz-result").text("snooty");
       $("#snooty").show();
       $("#submit").toggle();
 
     }
-    else if (questionOutput <= 35 && questionOutput > 22){
+    else if (result <= 35 && result > 22){
       $(".results").show();
       $("#quiz-result").text("cranky");
       $("#cranky").show();
       $("#submit").toggle();
 
     }
-    else if (questionOutput <= 49 && questionOutput > 36){
+    else if (result <= 49 && result > 36){
       $(".results").show();
       $("#quiz-result").text("jock");
       $("#jock").show();
