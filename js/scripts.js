feb6f5a3bef0ff0957f599cba2cf798fa62986e6
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-25 16:30:29 -0700

    update functions to split back and front end

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,41 @@
+var addInput = function(input1, input2, input3, input4, input5, input6, input7) {
+  return input1 + input2 + input3 + input4 + input5 + input6 + input7;
+}
+
 $(document).ready(function() {
   $("#submit").click(function(){
-    var result = parseInt($("input:radio.question1:checked").val()) + parseInt($("input:radio.question2:checked").val()) + parseInt($("input:radio.question3:checked").val()) + parseInt($("input:radio.question4:checked").val()) + parseInt($("input:radio.question5:checked").val()) + parseInt($("input:radio.question6:checked").val()) + parseInt($("input:radio.question7:checked").val());
-    console.log(result);
+    var q1 = parseInt($("input:radio.question1:checked").val());
+    var q2 = parseInt($("input:radio.question2:checked").val());
+    var q3 = parseInt($("input:radio.question3:checked").val());
+    var q4 = parseInt($("input:radio.question4:checked").val());
+    var q5 = parseInt($("input:radio.question5:checked").val());
+    var q6 = parseInt($("input:radio.question6:checked").val());
+    var q7 = parseInt($("input:radio.question7:checked").val());
+    
+    var total = addInput(q1, q2, q3, q4, q5, q6, q7);
 
-    if (result <= 7){
+    if (total <= 7){
       $(".results").show();
       $("#quiz-result").text("lazy");
       $("#lazy").show();
       $("#submit").toggle();
 
     }
-    else if (result <= 21 && result > 8){
+    else if (total <= 21 && total > 8){
       $(".results").show();
       $("#quiz-result").text("snooty");
       $("#snooty").show();
       $("#submit").toggle();
 
     }
-    else if (result <= 35 && result > 22){
+    else if (total <= 35 && total > 22){
       $(".results").show();
       $("#quiz-result").text("cranky");
       $("#cranky").show();
       $("#submit").toggle();
 
     }
-    else if (result <= 49 && result > 36){
+    else if (total <= 49 && total > 36){
       $(".results").show();
       $("#quiz-result").text("jock");
       $("#jock").show();
